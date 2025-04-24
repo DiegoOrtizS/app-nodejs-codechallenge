3cb90e795f7192158ea6f8dacefd1f5ba95c1baa
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2025-04-23 23:02:01 -0500

    feat: remove unnecesary use cases

--- apps/transaction-service/src/app.module.ts
+++ apps/transaction-service/src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TransactionPrismaRepository } from './infrastructure/repositories/transaction-prisma.repository';
 import { TransactionController } from './interface/controllers/transaction.controller';
-import { CreateTransactionUseCase } from './application/use-cases/create-transaction.use-case';
-import { GetTransactionUseCase } from './application/use-cases/get-transaction.use-case';
-import { PrismaModule } from './infrastructure/repositories/prisma.module'; // Import PrismaModule
+import { PrismaModule } from './infrastructure/repositories/prisma.module';
 import { TransactionService } from './application/services/transaction.service';
 
 @Module({
-  imports: [PrismaModule], // Ensure PrismaModule is imported
+  imports: [PrismaModule],
   controllers: [TransactionController],
   providers: [
     TransactionService,
-    CreateTransactionUseCase,
-    GetTransactionUseCase,
     {
       provide: 'TransactionRepositoryPort',
       useClass: TransactionPrismaRepository,
--- apps/transaction-service/src/interface/controllers/transaction.controller.ts
+++ apps/transaction-service/src/interface/controllers/transaction.controller.ts
@@ -1,23 +1,21 @@
 import { Controller, Post, Get, Param, Body } from '@nestjs/common';
-import { CreateTransactionUseCase } from '../../application/use-cases/create-transaction.use-case';
-import { GetTransactionUseCase } from '../../application/use-cases/get-transaction.use-case';
 import { Transaction } from '../../domain/entities/transaction.entity';
 import { CreateTransactionDto } from '../../dto/create-transaction.dto';
+import { TransactionService } from '../../application/services/transaction.service';
 
 @Controller('transactions')
 export class TransactionController {
   constructor(
-    private readonly createTransactionUseCase: CreateTransactionUseCase,
-    private readonly getTransactionUseCase: GetTransactionUseCase,
+    private readonly transactionService: TransactionService,
   ) {}
 
   @Post()
   async create(@Body() dto: CreateTransactionDto): Promise<Transaction> {
-    return this.createTransactionUseCase.execute(dto);
+    return this.transactionService.createTransaction(dto);
   }
 
   @Get(':transactionExternalId')
   async get(@Param('transactionExternalId') id: string): Promise<Transaction | null> {
-    return this.getTransactionUseCase.execute(id);
+    return this.transactionService.getTransactionById(id);
   }
 }
